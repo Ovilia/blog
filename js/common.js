6ad84bf6f28aa149dde2f679eb636e0d877280b6
Wenli Zhang <zwl.sjtu@gmail.com>
2013-10-19 22:37:44 +0800

    Yet another attempt to change base url

--- js/common.js
+++ js/common.js
@@ -1,13 +1,13 @@
 window.onload = function() {
     // lazy load js and css
     LazyLoad.js('http://zhangwenli.com/js/jquery-1.9.0.js', function () {
-        LazyLoad.js('js/unviel.min.js', function () {
+        LazyLoad.js('http://zhangwenli.com/blog/js/unviel.min.js', function () {
             $("img").unveil();
         });
     });
     // emoji
-    LazyLoad.css('css/emojify.min.css', function () {
-        LazyLoad.js('js/emojify.min.js', function () {
+    LazyLoad.css('http://zhangwenli.com/blog/css/emojify.min.css', function () {
+        LazyLoad.js('http://zhangwenli.com/blog/js/emojify.min.js', function () {
             emojify.setConfig({
                 emoticons_enabled: true,
                 people_enabled: true,
