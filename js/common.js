838750c07dbe694c613943caf53d14a291ca9b0c
Ovilia <zwl.sjtu@gmail.com>
2014-07-05 14:36:20 +0800

    window onload delete

--- js/common.js
+++ js/common.js
@@ -1,19 +1,17 @@
-window.onload = function() {
-    // lazy load js and css
-    LazyLoad.js('http://zhangwenli.com/blog/js/jquery-1.11.1.min.js', function () {
-        LazyLoad.js('/blog/js/unviel.min.js', function () {
-            $("img").unveil();
-        });
-    });
-    // emoji
-    LazyLoad.css('/blog/css/emojify.min.css', function () {
-        LazyLoad.js('/blog/js/emojify.min.js', function () {
-            emojify.setConfig({
-                emoticons_enabled: true,
-                people_enabled: true,
-                nature_enabled: true
-            });
-            emojify.run();
-        });
-    });
-};
+// lazy load js and css
+LazyLoad.js('http://zhangwenli.com/blog/js/jquery-1.11.1.min.js', function () {
+	LazyLoad.js('/blog/js/unviel.min.js', function () {
+		$("img").unveil();
+	});
+});
+// emoji
+LazyLoad.css('/blog/css/emojify.min.css', function () {
+	LazyLoad.js('/blog/js/emojify.min.js', function () {
+		emojify.setConfig({
+			emoticons_enabled: true,
+			people_enabled: true,
+			nature_enabled: true
+		});
+		emojify.run();
+	});
+});
