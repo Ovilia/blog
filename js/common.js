e6adcb35a3eaf8f543c4105b6cba615dd4d72acd
ovilia <zwl.sjtu@gmail.com>
2014-07-29 21:16:01 +0800

    test: lazy load emoji

--- js/common.js
+++ js/common.js
@@ -1,17 +1,20 @@
 // lazy load js and css
 LazyLoad.js('http://zhangwenli.com/blog/js/jquery-1.11.1.min.js', function () {
-	LazyLoad.js('/blog/js/unviel.min.js', function () {
-		$("img").unveil();
-	});
+    LazyLoad.js('/blog/js/unviel.min.js', function () {
+            $("img").unveil();
+    });
 });
-// emoji
-LazyLoad.css('/blog/css/emojify.min.css', function () {
-	LazyLoad.js('/blog/js/emojify.min.js', function () {
-		emojify.setConfig({
-			emoticons_enabled: true,
-			people_enabled: true,
-			nature_enabled: true
-		});
-		emojify.run();
-	});
-});
+setTimeout(function() {
+    // emoji
+    LazyLoad.css('/blog/css/emojify.min.css', function () {
+        LazyLoad.js('/blog/js/emojify.min.js', function () {
+            emojify.setConfig({
+                emoticons_enabled: true,
+                people_enabled: true,
+                nature_enabled: true
+            });
+            emojify.run();
+        });
+    });
+    console.log('lazy');
+}, 10000);
