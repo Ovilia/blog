58af1a7b12b94be68287059894e0e0acd491f0ef
wenlizha <wenli.zhang@intel.com>
2014-08-04 16:58:15 +0800

    use google fonts

--- js/common.js
+++ js/common.js
@@ -1,19 +1,25 @@
 // lazy load js and css
-LazyLoad.js('http://zhangwenli.com/blog/js/jquery-1.11.1.min.js', function () {
+LazyLoad.js('/blog/js/jquery-1.11.1.min.js', function () {
     LazyLoad.js('/blog/js/unviel.min.js', function () {
         $("img").unveil();
     });
 });
-setTimeout(function() {
-    // emoji
-    LazyLoad.css('/blog/css/emojify.min.css', function () {
-        LazyLoad.js('/blog/js/emojify.min.js', function () {
-            emojify.setConfig({
-                emoticons_enabled: true,
-                people_enabled: true,
-                nature_enabled: true
-            });
-            emojify.run();
+// emoji
+LazyLoad.css('/blog/css/emojify.min.css', function () {
+    LazyLoad.js('/blog/js/emojify.min.js', function () {
+        emojify.setConfig({
+            emoticons_enabled: true,
+            people_enabled: true,
+            nature_enabled: true
         });
+        emojify.run();
     });
-}, 10000);
+});
+// google fonts
+var url = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+WebFontConfig = {
+    google: { families: [ 'Lato:400,900:latin' ] }
+};
+LazyLoad.js(url);
+
