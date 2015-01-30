284e4b0b7f38ea86a42d6556ebcd14283eb59e74
Wenli Zhang <zwl.sjtu@gmail.com>
2015-01-30 12:22:19 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
+var isFirstToggleMusic = true;
 function toggleMusicPanel() {
     $('#music-control').toggleClass('on');
+    if (isFirstToggleMusic) {
+        _gaq.push(['_trackEvent', 'ToggleMusic', 'InRecent', window.location.pathname]);
+        isFirstToggleMusic = false;
+    }
 }
 
 function processPageView(rows) {
