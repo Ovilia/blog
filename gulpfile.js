39c2de7f9eb6d6b0e8c57849609c0beb869aa21b
Ovilia <zwl.sjtu@gmail.com>
2015-10-27 19:17:34 +0800

    add gulp for project

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+
+/**
+ * Compile files from _style into css
+ */
+gulp.task('sass', function () {
+    return gulp.src('_style/general.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: function(e) {
+                console.error(e);
+            },
+            outputStyle: 'compressed'
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(rename('default.min.css'))
+        .pipe(gulp.dest('css'));
+});
+
+/**
+ * Minify js
+ */
+gulp.task('compress', function() {
+    return gulp.src('js/common.js')
+        .pipe(uglify())
+        .pipe(rename('common.min.js'))
+        .pipe(gulp.dest('js'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_style/*.scss', ['sass']);
+    gulp.watch('js/common.js', ['compress']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['compress', 'sass', 'watch']);
